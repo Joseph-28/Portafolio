1b56e095865f91ea388154b4b4dff8a48c661f05
Joseph Gonzalez Herrera <goteks28@gmail.com>
2025-12-10 15:57:03 -0600

    Contenido del main

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggles = document.querySelectorAll("[data-toggle]");
+
+    toggles.forEach(header => {
+        header.addEventListener("click", () => {
+
+            const section = header.closest(".section");
+            const id = header.getAttribute("data-toggle");
+            const content = document.getElementById(id);
+
+            // Toggle estados
+            section.classList.toggle("open");
+            header.classList.toggle("open");
+            content.classList.toggle("open");
+        });
+    });
+});
